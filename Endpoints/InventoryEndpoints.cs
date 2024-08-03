ef7d8a358b56524e1d835af743a6c497b789da51
bearismybrother <thetwai123456789@gmail.com>
2024-08-02 20:07:27 -0700

    Organized the endpoints to their respective services

---
+++ Endpoints/InventoryEndpoints.cs
@@ -0,0 +1,125 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Json;
+using Marketplace_API_Gateway.DTOs;
+using MarketPlace_API_Gateway.DTOs;
+using MarketPlace_API_Gateway.Messaging_Queue;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketPlace_API_Gateway.Endpoints
+{
+    public static class InventoryEndpoints
+    {
+        public static void MapInventoryEndpoints(this WebApplication app)
+        {
+            var group = app.MapGroup("Inventory").RequireAuthorization().WithParameterValidation();
+            //GET all items in Inventory
+            group.MapGet(
+                "/",
+                async (HttpClient httpClient) =>
+                {
+                    //replace this with an url from appsetting.json or dotenv with CORS set up to only accept that incoming url
+                    var response = await httpClient.GetAsync("https://localhost:7194/Products");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return Results.Ok(response.Content.ReadAsStringAsync());
+                    }
+                    else
+                    {
+                        return Results.StatusCode(503);
+                    }
+                }
+            );
+
+            //GET Inventory by ID
+            group.MapGet(
+                "/{id}",
+                async (HttpClient httpClient, int id) =>
+                {
+                    var response = await httpClient.GetAsync(
+                        Environment.GetEnvironmentVariable("InventoryServiceURL") + id
+                    );
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return Results.Ok(response.Content.ReadAsStringAsync());
+                    }
+                    else
+                    {
+                        return Results.StatusCode(503);
+                    }
+                }
+            );
+
+            //POST new item to Inventory
+            group.MapPost(
+                "/New",
+                (CreateProductDTO newProduct, HttpContext httpContext, IQueueMethods queue) =>
+                {
+                    EndpointsHelperMethods.AddUserIdToPostedBy(httpContext, newProduct);
+                    queue.SendTask("CREATE", JsonSerializer.Serialize(newProduct), "Inventory");
+                }
+            );
+
+            //PUT update item in Inventory
+            group.MapPut(
+                "/Update/{id}",
+                (
+                    [FromBody] UpdateProductDTO updatedProduct,
+                    HttpContext httpContext,
+                    IQueueMethods queue,
+                    int id
+                ) =>
+                {
+                    EndpointsHelperMethods.AddUserIdToPostedBy(httpContext, updatedProduct);
+                    updatedProduct.Id = id;
+                    queue.SendTask("UPDATE", JsonSerializer.Serialize(updatedProduct), "Inventory");
+                }
+            );
+
+            //DELETE item in Inventory
+            group.MapDelete(
+                "/Delete/{id}",
+                (
+                    [FromBody] DeleteProductDTO deleteProduct,
+                    HttpContext httpContext,
+                    IQueueMethods queue,
+                    int id
+                ) =>
+                {
+                    EndpointsHelperMethods.AddUserIdToPostedBy(httpContext, deleteProduct);
+                    deleteProduct.Id = id;
+                    queue.SendTask("DELETE", JsonSerializer.Serialize(deleteProduct), "Inventory");
+                }
+            );
+
+            //POST search for item in Inventory
+            group.MapPost(
+                "/query",
+                (
+                    [FromQuery] [Required] string keyword,
+                    [FromQuery] int minPrice,
+                    HttpClient httpClient
+                ) =>
+                {
+                    if (keyword == null)
+                    {
+                        return Results.BadRequest("Keyword is required");
+                    }
+                    string[] breakWords = ["the", "a", "is", "on"];
+                    StringBuilder sb = new(keyword);
+                    foreach (string breakWord in breakWords)
+                    {
+                        sb.Replace(breakWord, "");
+                    }
+                    var queries = httpClient.PostAsJsonAsync(
+                        Environment.GetEnvironmentVariable("InventoryServiceURL") + "query",
+                        sb.ToString()
+                    );
+                    return Results.Ok(queries);
+                    //    .split(/\s+/)
+                    //    .join("|");
+                }
+            );
+        }
+    }
+}
---
+++ Endpoints/ShoppingCartEndpoints.cs
@@ -0,0 +1,109 @@
+﻿using System.Text.Json;
+using Marketplace_API_Gateway.DTOs;
+using MarketPlace_API_Gateway.DTOs.Shopping_Cart_Services;
+using MarketPlace_API_Gateway.Messaging_Queue;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketPlace_API_Gateway.Endpoints
+{
+    public static class ShoppingCartEndpoints
+    {
+        public static void MapShoppingCartEndpoints(this WebApplication app)
+        {
+            var group = app.MapGroup("ShoppingCart")
+                .RequireAuthorization()
+                .WithParameterValidation();
+            // GET all products in the shopping cart
+            group.MapGet(
+                "/{id}",
+                async (HttpClient httpClient, int id) =>
+                {
+                    var shoppingCartResponse = await httpClient.GetAsync(
+                        Environment.GetEnvironmentVariable("ShoppingCartServiceURL") + id
+                    );
+
+                    if (shoppingCartResponse.IsSuccessStatusCode)
+                    {
+                        var productIdsJson = shoppingCartResponse
+                            .Content.ReadAsStringAsync()
+                            .Result;
+                        var productIds = JsonSerializer.Deserialize<List<int>>(productIdsJson);
+                        var tasks = new List<Task<HttpResponseMessage>>();
+                        foreach (var productId in productIds)
+                        {
+                            tasks.Add(
+                                httpClient.GetAsync(
+                                    Environment.GetEnvironmentVariable("InventoryServiceURL")
+                                        + productId
+                                )
+                            );
+                        }
+                        var inventoryResponse = await Task.WhenAll(tasks);
+                        List<ProductSummaryDTO> products = new();
+                        foreach (var response in inventoryResponse)
+                        {
+                            if (response.IsSuccessStatusCode)
+                            {
+                                var productJson = response.Content.ReadAsStringAsync().Result;
+                                ProductSummaryDTO product =
+                                    JsonSerializer.Deserialize<ProductSummaryDTO>(
+                                        productJson,
+                                        new JsonSerializerOptions
+                                        {
+                                            //to deal with psql returning only lower case
+                                            PropertyNameCaseInsensitive = false
+                                        }
+                                    );
+                                products.Add(product);
+                            }
+                        }
+                        return Results.Ok(products);
+                    }
+                    else
+                    {
+                        return Results.StatusCode(503);
+                    }
+                }
+            );
+
+            //POST new item to Shopping Cart
+            group.MapPost(
+                "/Add",
+                (AddToShoppingCartDTO newProduct, HttpContext httpContext, IQueueMethods queue) =>
+                {
+                    EndpointsHelperMethods.AddUserIdToPostedBy(httpContext, newProduct);
+                    queue.SendTask("CREATE", JsonSerializer.Serialize(newProduct), "ShoppingCart");
+                }
+            );
+
+            //DELETE item from Shopping Cart
+            group.MapDelete(
+                "/Remove/{id}",
+                (
+                    [FromBody] RemoveFromShoppingCartDTO removeProduct,
+                    HttpContext httpContext,
+                    IQueueMethods queue,
+                    int id
+                ) =>
+                {
+                    EndpointsHelperMethods.AddUserIdToPostedBy(httpContext, removeProduct);
+                    removeProduct.ItemId = id;
+                    queue.SendTask(
+                        "DELETE",
+                        JsonSerializer.Serialize(removeProduct),
+                        "ShoppingCart"
+                    );
+                }
+            );
+
+            //POST Finilize Shopping Cart to checkout
+            group.MapPost(
+                "/Checkout",
+                () => {
+                    // 1) Confirm the payment has been made
+                    // 2) Send inventory, shopping cart, and emailing services their tasks in the messaging queues
+                }
+            );
+        }
+    }
+}
